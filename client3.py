068606a4f7205663e995f07250dc7a0103503909
cakejune <tyq444@gmail.com>
2023-11-13 15:03:20 -0500

    completed assignment with bonus challenge (unit tests).

--- client3.py
+++ client3.py
@@ -35,14 +35,20 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    #python3 automatically uses float calculation with the '/' operator so float defining is unecessary
+    price = (bid_price + ask_price) / 2 
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if not isinstance(price_a, (float, int)) or not isinstance(price_b, (float, int)):
+        raise TypeError("Values must be either integer or float type.")
+    if price_b == 0:
+        raise ValueError("Value undefined. Divided by 0")
+    
+    return price_a/price_b
 
 
 # Main
@@ -52,8 +58,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        #creating a dictionary to store the price ratios
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["JPM"], prices["MSFT"]))
